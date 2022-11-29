36a9c8ca9b95bd47790a9831c98ba89c933a58ab
zelo0 <minorSunbi@gmail.com>
2022-11-29 23:24:57 +0900

    refactor: 숫자 검증이 아닌 문자열 자체로 입력 검증

--- src/main/java/subway/domain/Validator.java
+++ src/main/java/subway/domain/Validator.java
@@ -2,29 +2,13 @@ package subway.domain;
 
 public class Validator {
 
-    public static void checkIfNumberInRange(String input, int startInclusive, int endInclusive) {
-        int inputNumber;
-
-        try {
-            inputNumber = Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
-        }
-
-        if (inputNumber < startInclusive || inputNumber > endInclusive) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    public static void checkIfValidInput(String input, int startInclusive, int endInclusive, String acceptedString) {
+    public static void checkIfValidInput(String input, String[] acceptedStringList) {
         String trimmedInput = input.trim();
-        if (trimmedInput.equalsIgnoreCase(acceptedString)) {
-            return;
+        for (String acceptedString : acceptedStringList) {
+            if (trimmedInput.equalsIgnoreCase(acceptedString)) {
+                return;
+            }
         }
-        try {
-            checkIfNumberInRange(trimmedInput, startInclusive, endInclusive);
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException();
-        }
+        throw new IllegalArgumentException();
     }
 }
