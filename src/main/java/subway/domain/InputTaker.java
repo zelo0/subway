1f41022ad32b4ba6fea6f7631468049469b517d5
zelo0 <minorSunbi@gmail.com>
2022-11-30 00:22:50 +0900

    feat: 역 추가, 삭제

--- src/main/java/subway/domain/InputTaker.java
+++ src/main/java/subway/domain/InputTaker.java
@@ -15,9 +15,15 @@ public class InputTaker {
         return scanner.nextLine();
     }
 
-    public String takeStationName() {
+    public String takeAddingStationName() {
         System.out.println();
         System.out.println("## 등록할 역 이름을 입력하세요.");
+        return scanner.nextLine();
+    }
+
+    public String takeDeletingStationName() {
+        System.out.println();
+        System.out.println("## 삭제할 역 이름을 입력하세요.");
         return scanner.nextLine();
     }
 }
--- src/main/java/subway/domain/controller/StationController.java
+++ src/main/java/subway/domain/controller/StationController.java
@@ -1,7 +1,9 @@
 package subway.domain.controller;
 
-import subway.domain.InputTaker;
-import subway.domain.Validator;
+import subway.domain.*;
+
+import java.util.List;
+import java.util.Map;
 
 public class StationController {
 
@@ -11,27 +13,88 @@ public class StationController {
     private static final String BACK_STR = "B";
 
     public static void run(InputTaker inputTaker) {
-        printMenu();
-        String menuInput = requestMenuSelection(inputTaker);
-        if (menuInput.equals(ENROLL_STR)) {
-            enrollStation(inputTaker);
+        while (true) {
+            printMenu();
+            String menuInput = requestMenuSelection(inputTaker);
+            if (menuInput.equals(ENROLL_STR) && enrollStation(inputTaker)) {
+                return;
+            }
+            if (menuInput.equals(DELETE_STR) && deleteStation(inputTaker)) {
+                return;
+            }
         }
     }
 
-    private static void enrollStation(InputTaker inputTaker) {
+    private static boolean deleteStation(InputTaker inputTaker) {
+        String inputName = inputTaker.takeDeletingStationName();
+        if (isAbleToDeleteStation(inputName)) {
+            StationRepository.deleteStation(inputName);
+            System.out.println();
+            System.out.println("[INFO] 지하철 역이 삭제되었습니다.");
+            System.out.println();
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isAbleToDeleteStation(String inputName) {
+        Map<Line, List<Station>> lineListMap = LineMapRepository.lineStations();
+        // 노선에 등록된 역은 삭제 불가
+        if (isEnrolledInLine(inputName, lineListMap)) {
+            return false;
+        }
+        // 존재하지 않는 역도 삭제 불가
+        return isAlreadyExistsStation(inputName);
+    }
+
+    private static boolean isAlreadyExistsStation(String inputName) {
+        List<Station> stations = StationRepository.stations();
+        for (Station station : stations) {
+            if (station.getName().equals(inputName)) {
+                return true;
+            }
+        }
+        System.out.println("[ERROR] 존재하지 않는 역입니다.");
+        return false;
+    }
+
+    private static boolean isEnrolledInLine(String inputName, Map<Line, List<Station>> lineListMap) {
+        for (List<Station> stationList : lineListMap.values()) {
+            if (containsStation(inputName, stationList)) {
+                System.out.println("[ERROR] 노선에 등록된 역은 삭제할 수 없습니다.");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean containsStation(String inputName, List<Station> stationList) {
+        for (Station station : stationList) {
+            if (station.getName().equals(inputName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean enrollStation(InputTaker inputTaker) {
         String inputName;
-        do {
-            inputName = inputTaker.takeStationName();
-        } while (!isValidStationName(inputName));
-        System.out.println();
-        System.out.println("[INFO] 지하철 역이 등록되었습니다.");
-        System.out.println();
+        inputName = inputTaker.takeAddingStationName();
+        if (isValidStationName(inputName)) {
+            StationRepository.addStation(new Station(inputName));
+            System.out.println();
+            System.out.println("[INFO] 지하철 역이 등록되었습니다.");
+            System.out.println();
+            return true;
+        }
+        return false;
     }
 
     private static boolean isValidStationName(String inputStationName) {
         try {
             Validator.checkIfValidStationName(inputStationName);
         } catch (IllegalArgumentException e) {
+            System.out.println();
             System.out.println("[ERROR] 지하철 역 이름은 2글자 이상이고 기존에 등록되지 않은 이름이어야 합니다.");
             System.out.println();
             return false;
@@ -60,6 +123,7 @@ public class StationController {
         try {
             Validator.checkIfValidMenu(input, new String[]{ENROLL_STR, DELETE_STR, VIEW_STR, BACK_STR});
         } catch (IllegalArgumentException e) {
+            System.out.println();
             System.out.println("[ERROR] 메뉴에서 선택해주세요");
             System.out.println();
             return false;
