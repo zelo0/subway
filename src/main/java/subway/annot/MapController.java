7be19c26483259eed9432a6fa98e231fba93b17d
zelo0 <minorSunbi@gmail.com>
2022-12-03 15:04:58 +0900

    style: rename Controller -> MapController

--- src/main/java/subway/annot/MapController.java
+++ src/main/java/subway/annot/MapController.java
@@ -5,7 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.TYPE)
+@Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Controller {
+public @interface MapController {
+    String value();
 }
